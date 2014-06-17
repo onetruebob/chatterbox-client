d8d7db6c1fdc084367cf6c270d01e59f5f5b1e34
Hack Reactor Students <bob@bobowen.org>
2014-06-16 17:10:16 -0700

    HTML encode message strings retrieved from the server before displaying them.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -19,11 +19,11 @@ app.send = function(message){
     data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
-      console.log('chatterbox: Message sent');////////////////////////////////////////////////
+      console.log('chatterbox: Message sent'); ////////////////////////////////////////////////
     },
     error: function (data) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');////////////////////////////////////////////////
+      console.error('chatterbox: Failed to send message'); ////////////////////////////////////////////////
     }
   });
 };
@@ -33,8 +33,9 @@ app.fetch = function(){
     url: app.server,
     type: 'GET',
     success: function (data) {
-      console.dir(data);  ////////////////////////////////////////////////
+      console.dir(data); ////////////////////////////////////////////////
       console.log('chatterbox: fetch completed.'); ////////////////////////////////////////////////
+
       app._renderMessages(data.results);
     },
     error: function (data){
@@ -50,15 +51,23 @@ app.fetch = function(){
 //     text: "hello"
 //     updatedAt: "2013-10-07T16:22:03.280Z"
 //     username: "gary"
+
 app._renderMessages = function(messages){
   _(messages).each(function(msgObj){
     app.$messages.append($(app._htmlFromMsgObj(msgObj)));
   });
 };
 
+app._htmlEncode = function(unencodedString){
+  return $('<div>').text(unencodedString).html();
+};
+
 // Given a message object, return an HTML string formatted for our app.
 app._htmlFromMsgObj = function(msgObj){
-  return '<div class="message"><p>'+msgObj.username+' said: '+msgObj.text+'<br>On: '+msgObj.createdAt+'</p></div>';
+  return '<div class="message"><p>'+app._htmlEncode(msgObj.username)+
+      ' said: '+app._htmlEncode(msgObj.text)+
+      '<br>On: '+app._htmlEncode(msgObj.createdAt)+
+      '</p></div>';
 };
 
 app.init();
